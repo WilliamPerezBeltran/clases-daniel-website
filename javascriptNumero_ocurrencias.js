792bd546185ee4b0b7ad180c69cbdf525bf3db19
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-01-30 10:59:33 -0500

    Add logic number of occurrences

---
+++ javascriptNumero_ocurrencias.js
@@ -0,0 +1,20 @@
+let numero = [1,2,3,4,5,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,100]
+let acurrencias=[]
+let valoresUnicos = numero.filter((currentValue,index,array)=> array.indexOf(currentValue)=== index )
+
+for (var i = 0; i < valoresUnicos.length; i++) {
+	let contador = 0
+	for (var j = 0; j < numero.length; j++) {
+
+		if( valoresUnicos[i] === numero[j] ){
+			contador++
+		}
+		
+	}	
+
+		acurrencias.push({numero:valoresUnicos[i],occurrencias:contador})
+}
+
+console.log('arrayPrimario',numero)
+console.log('valoresUnicos',valoresUnicos)
+console.log('acurrencias',acurrencias)
