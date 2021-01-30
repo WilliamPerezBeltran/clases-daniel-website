80aa5f17f5a11b7ee2127e716548bf68d4d8bc61
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-01-30 12:26:54 -0500

    add package json

--- javascriptNumero_ocurrencias.js
+++ javascriptNumero_ocurrencias.js
@@ -1,21 +1,50 @@
-let numero = [1,2,3,4,5,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,100]
-let acurrencias=[]
-let valoresUnicos = numero.filter((currentValue,index,array)=> array.indexOf(currentValue)=== index )
+let numero = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  9,
+  100,
+];
+let acurrencias = [];
+let valoresUnicos = numero.filter(
+  (currentValue, index, array) => array.indexOf(currentValue) === index
+);
 
 for (var i = 0; i < valoresUnicos.length; i++) {
-	let contador = 0
-	for (var j = 0; j < numero.length; j++) {
+  let contador = 0;
+  for (var j = 0; j < numero.length; j++) {
+    if (valoresUnicos[i] === numero[j]) {
+      contador++;
+    }
+  }
 
-		if( valoresUnicos[i] === numero[j] ){
-			contador++
-		}
-		
-	}	
-
-		// acurrencias.push({numero:valoresUnicos[i],occurrencias:contador})
-		acurrencias.push(contador)
+  // acurrencias.push({numero:valoresUnicos[i],occurrencias:contador})
+  acurrencias.push(contador);
 }
 
-console.log('arrayPrimario',numero)
-console.log('valoresUnicos',valoresUnicos)
-console.log('acurrencias',acurrencias)
+console.log("arrayPrimario", numero);
+console.log("valoresUnicos", valoresUnicos);
+console.log("acurrencias", acurrencias);
--- matriz.js
+++ matriz.js
@@ -1,49 +1,35 @@
-let matriz = [[4,3,2,8],[3,1,4,2],[8,5,3,2],[2,1,8,3]]
+let matriz = [
+  [4, 3, 2, 8],
+  [3, 1, 4, 2],
+  [8, 5, 3, 2],
+  [2, 1, 8, 3],
+];
 
-// vectorNuevo = []
-// nuevoArray = []
 for (var i = 0; i < matriz.length; i++) {
-	nuevoArray = []
-	for (var j = 0; j < matriz[i].length; j++) {
-
-		// console.log(matriz[i][j])
-		// console.log(matriz[j][i])
-		nuevoArray.push({fila:i,columna:j,dato:matriz[j][i]})
-		
-	}	
-	console.log('\n')
-	construirRespuesta(nuevoArray)
-
-		
-}
-// console.log('nuevoArray',nuevoArray)
-
-function construirRespuesta(columnaArray){
-	// console.log('nuevoArray',columnaArray)
-	let rta
-	let  numeroMinimo = columnaArray[0].dato
-	for (var i = 0; i < columnaArray.length; i++) {
-		if (columnaArray[i].dato < numeroMinimo){
-			numeroMinimo = nuevoArray[i].dato
-			rta = [{minimo:numeroMinimo,fila:columnaArray[i].columna+1,columna:columnaArray[i].fila+1}]
-
-		}
-	}
-	console.log('rta',rta)
-	return rta
-
-
+  nuevoArray = [];
+  for (var j = 0; j < matriz[i].length; j++) {
+    nuevoArray.push({ fila: i, columna: j, dato: matriz[j][i] });
+  }
+  console.log("\n");
+  construirRespuesta(nuevoArray);
 }
 
+function construirRespuesta(columnaArray) {
+  let rta;
+  let numeroMinimo = columnaArray[0].dato;
+  for (var i = 0; i < columnaArray.length; i++) {
+    if (columnaArray[i].dato < numeroMinimo) {
+      numeroMinimo = nuevoArray[i].dato;
 
-
-// nuevoArray = [4,3,8,2]
-// let  numeroMinimo = nuevoArray[0]
-// for (var i = 0; i < nuevoArray.length; i++) {
-// 	if (nuevoArray[i]<numeroMinimo){
-// 		numeroMinimo = nuevoArray[i]
-
-// 	}
-// }
-// console.log('nuevoArray',nuevoArray)
-// console.log('numeroMinimo',numeroMinimo)
+      rta = [
+        {
+          minimo: numeroMinimo,
+          fila: columnaArray[i].columna + 1,
+          columna: columnaArray[i].fila + 1,
+        },
+      ];
+    }
+  }
+  console.log("rta", rta);
+  return rta;
+}
